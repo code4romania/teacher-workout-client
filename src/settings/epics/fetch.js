a2c53c755eb34676ab0ac4bc4c317dbbcf4aafcf
Radu B <radu.buciuceanu@gmail.com>
2021-06-25 21:15:23 +0300

    [feature-15] Create fetch epic

---
+++ src/settings/epics/fetch.js
@@ -0,0 +1,13 @@
+import { ofType } from 'redux-observable'
+import { catchError, map, mergeMap } from 'rxjs/operators'
+
+import { actions } from 'settings/slices/settings'
+import * as repository from 'settings/repositories/fetch'
+
+const perform = () => repository.fetch()
+  .pipe(map(actions.success))
+  .pipe(catchError(error => actions.failure(error)))
+
+export const fetch = action$ => action$
+  .pipe(ofType(actions.attempt.type))
+  .pipe(mergeMap(perform))
