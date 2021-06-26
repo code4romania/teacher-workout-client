da4be3f077fc2bbf4b61f9fcecf837305ef14cd7
Radu B <radu.buciuceanu@gmail.com>
2021-06-26 14:36:08 +0300

    [feature-30] Change mobile icon

--- src/header/mobile-buttons/index.jsx
+++ src/header/mobile-buttons/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Apps } from '@material-ui/icons'
+import { Menu } from '@material-ui/icons'
 import { IconButton } from '@material-ui/core'
 
 import { Layout } from 'header/mobile-buttons/layout'
 
 export const MobileButtons = () => <Layout>
   <IconButton>
-    <Apps />
+    <Menu />
   </IconButton>
 </Layout>
