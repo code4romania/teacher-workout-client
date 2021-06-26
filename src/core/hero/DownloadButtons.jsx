d6651ccb3fa0586be622974574f8ff6d72e7ed91
Terzea Bogdan <terzeabogdan2007@yahoo.com>
2021-06-26 17:07:44 +0300

    Remove useless defined Box

--- src/core/hero/DownloadButtons.jsx
+++ src/core/hero/DownloadButtons.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Grid } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 
 import { AppleDownloadButton } from './AppleDownloadButton'
 import { GoogleDownloadButton } from './GoogleDownloadButton'
