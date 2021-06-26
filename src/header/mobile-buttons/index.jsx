1010faeca0c530130fe4273a8605f0d63fc7b3fd
Radu B <radu.buciuceanu@gmail.com>
2021-06-26 16:01:11 +0300

    [feature-30] Change mobile buttons

--- src/header/mobile-buttons/index.jsx
+++ src/header/mobile-buttons/index.jsx
@@ -3,9 +3,10 @@ import { Menu } from '@material-ui/icons'
 import { IconButton } from '@material-ui/core'
 
 import { Layout } from 'header/mobile-buttons/layout'
+import { useToggleCallbacks } from 'header/mobile-buttons/hooks/use-toggle-callbacks'
 
 export const MobileButtons = () => <Layout>
-  <IconButton>
+  <IconButton onClick={useToggleCallbacks()}>
     <Menu />
   </IconButton>
 </Layout>
