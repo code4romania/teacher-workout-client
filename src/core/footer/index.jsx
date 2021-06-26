44d6588840b079aa8cd196206865199960ffac28
Filip Taifas <andreifilip123@gmail.com>
2021-06-26 17:09:01 +0300

    Add second part of footer

--- src/core/footer/index.jsx
+++ src/core/footer/index.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import { Grid } from '@material-ui/core'
+import { Box, Grid, Typography } from '@material-ui/core'
 
 import { OrganizerLogo } from 'core/footer/organizer-logo'
 import { DownloadApp } from 'core/footer/download-app'
+import { DesktopButtons } from 'core/footer/desktop-buttons'
 
 export const Footer = () => (
-  <Grid container alignItems={"center"} justify={"space-between"}>
-    <OrganizerLogo />
-    <DownloadApp />
-  </Grid>
+  <Box>
+    <Box mb={4}>
+    <Grid container alignItems={"center"} justify={"space-between"}>
+        <OrganizerLogo />
+        <DownloadApp />
+    </Grid>
+    </Box>
+    <Box mb={4}>
+      <Grid container alignItems={"center"} justify={"space-between"}>
+          <Grid item>
+            <Typography>© 2021 Code for Romania. Toate drepturile rezervate.</Typography>
+          </Grid>
+          <Grid item>
+            <DesktopButtons />
+          </Grid>
+      </Grid>
+    </Box>
+  </Box>
 )
