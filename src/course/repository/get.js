283fe7d59b9a2d02b4ccb6a0c20df38de5e684d0
miutamihai <miuta.mihai@gmail.com>
2021-06-26 14:32:24 +0300

    [connect-backend] Change repository

--- src/course/repository/get.js
+++ src/course/repository/get.js
@@ -1,7 +1,7 @@
 import { map } from 'rxjs/operators'
 
-import {graph} from 'network'
-import {lessons} from 'course/repository/queries/lessons'
+import { graph } from 'network'
+import { lessons } from 'course/repository/queries/lessons'
 
-export const get = () => graph().query(lessons)
-    .pipe(map(({data: {lessons}}) => lessons))
+export const get = () => graph().query({ query: lessons })
+  .pipe(map(({ data: { lessons } }) => lessons))
