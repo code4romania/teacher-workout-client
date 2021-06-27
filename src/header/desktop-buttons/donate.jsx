4fcd37349af4c5f4691b8b88dd4a6171bece9923
Alin Bogdan <alin.bogdan@whiteboxideas.com>
2021-06-27 19:49:11 +0300

    [feature-5] fix donate hover

--- src/header/desktop-buttons/donate.jsx
+++ src/header/desktop-buttons/donate.jsx
@@ -3,12 +3,15 @@ import { Button, Grid, makeStyles } from '@material-ui/core'
 
 import { actions } from 'header/constants'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
-    background: 'green',
+    background: theme.palette.success.main,
     color: 'white',
+    '&:hover': { background: theme.palette.success.light },
+
   },
-})
+
+}))
 
 export const Donate = () => <Grid item md={3} lg={3} xl={3}>
   <Button className={useStyles().root}>{actions.donate}</Button>
