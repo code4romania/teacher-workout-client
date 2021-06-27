71ea44bd45fed4438440235dd68adfbd613f04b5
Alin Bogdan <alin.bogdan@whiteboxideas.com>
2021-06-27 19:49:53 +0300

    [feature-5] move card to home

---
+++ src/home/cards/a-card/call-button.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Button, makeStyles } from '@material-ui/core'
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    fontFamily: 'IBM Plex Sans',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: '16px',
+    lineHeight: 1.5,
+    color: '#FFFFFF',
+    marginTop: theme.spacing(3),
+    textTransform: 'none',
+
+  },
+}))
+export const CallButton = () => (
+  <Button variant={'contained'}
+          color={'primary'}
+          size={'small'}
+          className={useStyles().button}
+          endIcon={<ArrowForwardIcon />}>
+    Call to action ptr creatori
+  </Button>
+)
