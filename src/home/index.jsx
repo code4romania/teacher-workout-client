cf0920ff491c35dca91711c8ad51af787bcc9797
Radu B <radu.buciuceanu@gmail.com>
2021-06-27 12:47:06 +0300

    [bug-40] Use index in hero

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Box, makeStyles } from '@material-ui/core'
 
 import { Header } from 'header'
-import { Hero } from 'home/hero/hero'
+import { Hero } from 'home/hero'
 import { PrimaryFeatures } from 'home/primary-features'
 
 const useStyles = makeStyles({ root: { background: '#E5E5E5' } })
