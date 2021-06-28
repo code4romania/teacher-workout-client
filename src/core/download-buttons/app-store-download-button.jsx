af13ede9c26d683f682c12f56adf261103da0115
Filip Taifas <andreifilip123@gmail.com>
2021-06-28 19:00:04 +0300

    Move download-buttons in core

--- src/core/download-buttons/app-store-download-button.jsx
+++ src/core/download-buttons/app-store-download-button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Box, Grid } from '@material-ui/core'
-import AppStoreDownload from 'footer/download-app/logo-download-ios.svg'
+import AppStoreDownload from 'core/download-buttons/logo-download-ios.svg'
 
 export const AppStoreDownloadButton = () => (
   <Grid item>
--- src/core/download-buttons/index.jsx
+++ src/core/download-buttons/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Grid, Box, makeStyles } from '@material-ui/core'
 
-import { AppStoreDownloadButton } from 'footer/download-app/app-store-download-button'
-import { GooglePlayDownloadButton } from 'footer/download-app/google-play-download-button'
+import { AppStoreDownloadButton } from 'core/download-buttons/app-store-download-button'
+import { GooglePlayDownloadButton } from 'core/download-buttons/google-play-download-button'
 
 const useStyles = makeStyles(theme => ({ container: { justifyContent: 'center' } }))
 
