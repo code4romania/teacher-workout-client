9e5f1ad96fd10ca5b27f37881fed3395427720c6
Radu B <radu.buciuceanu@gmail.com>
2021-06-29 16:02:29 +0300

    [enh-52] Change hero spacing

--- src/home/hero/description/desktop.jsx
+++ src/home/hero/description/desktop.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
-import { Box, Hidden, Typography } from '@material-ui/core'
+import { Box, Hidden, makeStyles, Typography } from '@material-ui/core'
 
 const CONTENT = `Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Pariatur minima sequi recusandae, porro maiores officia assumenda aliquam laborum
           ab aliquid veritatis impedit odit adipisci optio iste blanditiis facere. Totam, velit.`
 
+const makeCommonStyles = theme => ({
+  width: 0.4,
+  paddingBottom: theme.spacing(2),
+  justifyContent: 'center',
+  alignContent: 'center',
+})
+
+const makeBreakpointStyles = theme => ({
+  [theme.breakpoints.only('xs')]: { width: '100%' },
+  [theme.breakpoints.only('sm')]: { width: '60%' },
+  [theme.breakpoints.only('md')]: { width: '50%' },
+  [theme.breakpoints.only('lg')]: { width: '40%' },
+  [theme.breakpoints.only('xl')]: { width: '50%' },
+})
+
+const useStyles = makeStyles(theme => ({
+  box: {
+    ...makeCommonStyles(theme),
+    ...makeBreakpointStyles(theme),
+  },
+}))
+
 export const Desktop = () => (
   <Hidden smDown>
     <Box display={'flex'} justifyContent={'center'}>
-      <Box alignContent={'center'} width={0.4} justifyContent={'center'} pb={2}>
+      <Box className={useStyles().box}>
         <Typography align={'center'}>{CONTENT}</Typography>
       </Box>
     </Box>
--- src/home/hero/description/mobile.jsx
+++ src/home/hero/description/mobile.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
-import { Box, Hidden, Typography } from '@material-ui/core'
+import { Box, Hidden, makeStyles, Typography } from '@material-ui/core'
 
 const CONTENT = `Aici ar fi un alt text care sa imputerniceasca ce zice 
 textul de mai sus si sa complementeze call 
 to action-ul pentru downloading-ul aplicatiei. pwp.`
 
+const useStyles = makeStyles(theme => ({
+  box: {
+    alignContent: 'center',
+    justifyContent: 'center',
+    paddingBottom: theme.spacing(2),
+    width: '80%',
+  },
+}))
+
 export const Mobile = () => (
   <Hidden mdUp>
     <Box display={'flex'} justifyContent={'center'}>
-      <Box alignContent={'center'}
-           width={4 / 5}
-           justifyContent={'center'}
-           pb={2}>
+      <Box className={useStyles().box}>
         <Typography align={'center'}>{CONTENT}</Typography>
       </Box>
     </Box>
