03d0ea0107c9ffc103dba0c92daa1af485abcb39
Radu B <radu.buciuceanu@gmail.com>
2021-06-29 15:59:24 +0300

    [enh-52] Change primary features spacing

--- src/home/features/primary/layout/spacing.jsx
+++ src/home/features/primary/layout/spacing.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Box, Grid } from '@material-ui/core'
+import { Box } from '@material-ui/core'
 
-export const Spacing = ({ children }) => (
-  <Box pt={10}>
-    <Grid container justify={'center'} alignItems={'center'}>
-      <Grid item xs={12} sm={11} md={10} lg={9} xl={9}>
-        {children}
-      </Grid>
-    </Grid>
-  </Box>
-)
+export const Spacing = ({ children }) => <Box pt={10}>
+  {children}
+</Box>
 
 Spacing.propTypes = { children: PropTypes.node.isRequired }
