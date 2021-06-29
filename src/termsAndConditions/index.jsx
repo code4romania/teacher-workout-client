8768c1771705606ec4b8c5281690953b3c35c292
Crisan Catalin <catacrisan_catacrsn@yahoo.com>
2021-06-29 11:12:46 +0300

    [feature-8] Static page component (#36)
    * TWC-8: Add Static Page component
    
    * TWC-8:  Add IBM Plex font
    
    * TWC-8: Change list style
    
    * TWC-8: New line
    
    * TWC-8: Fix some lints
    
    * TWC-8: Fix all lints
    
    * TWC-8: Update lock file
    
    Co-authored-by: Catalin <c>
    Co-authored-by: Radu B <radu.buciuceanu@gmail.com>

---
+++ src/termsAndConditions/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import { Header } from 'header'
+import { StaticPage } from 'core/staticpage/staticpage'
+
+export const TermsAndConditions = () => <>
+  <Header />
+  <StaticPage title={'Termeni și condiții'} articles={articles} />
+</>
+
+/* eslint max-len: 0 */
+const articles = [
+  {
+    title: '',
+    message: `<p>Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget. Eleifend egestas fringilla sapien.</p>
+          <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
+          <ul>
+              <li>Quis elit egestas venenatis mattis dignissim.</li>
+              <li>Quis elit egestas venenatis mattis dignissim.</li>
+              <li>Quis elit egestas venenatis mattis dignissim.</li>
+          </ul>
+          <p>Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit viverra aliquam porttitor ultrices gravida eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed elit interdum dignissim.</p>`,
+  },
+  {
+    title: '<h3>From beginner to expert in 30 days</h3>',
+    message: `<p>Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget. Eleifend egestas fringilla sapien.</p>
+          <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
+          <ul>
+              <li>Quis elit egestas venenatis mattis dignissim.</li>
+              <li>Quis elit egestas venenatis mattis dignissim.</li>
+              <li>Quis elit egestas venenatis mattis dignissim.</li>
+          </ul>
+          <p>Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit viverra aliquam porttitor ultrices gravida eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed elit interdum dignissim.</p>
+          <p>Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget. Eleifend egestas fringilla sapien.</p>
+          <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
+          <ul>
+              <li>Quis elit egestas venenatis mattis dignissim.</li>
+              <li>Quis elit egestas venenatis mattis dignissim.</li>
+              <li>Quis elit egestas venenatis mattis dignissim.</li>
+          </ul>
+          <p>Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit viverra aliquam porttitor ultrices gravida eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed elit interdum dignissim.</p>`,
+  },
+]
