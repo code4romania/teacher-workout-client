37eda248c81ad76cda581ee235dc49287d6faade
Radu B <radu.buciuceanu@gmail.com>
2021-06-29 11:21:19 +0300

    [feature-48] Create new routes

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -1,8 +1,18 @@
 import { Home } from 'home'
+import { TermsAndConditions } from 'termsAndConditions'
+import { PrivacyPolicy } from 'footer/nav-buttons/privacy-policy'
 
 export const routes = [
   {
     path: '/',
     Component: Home,
+  },
+  {
+    path: '/terms-and-conditions',
+    Component: TermsAndConditions,
+  },
+  {
+    path: '/privacy-policy',
+    Component: PrivacyPolicy,
   },
 ]
