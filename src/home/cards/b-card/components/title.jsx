be327f12b91c22dac8b8b748d99ef240e24bcb95
Radu B <radu.buciuceanu@gmail.com>
2021-06-30 17:17:34 +0300

    [feature-6] Code review follow up

--- src/home/cards/b-card/components/title.jsx
+++ src/home/cards/b-card/components/title.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
-import { Box, Typography } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
+import { Box, Typography, makeStyles } from '@material-ui/core'
 
 const primary = {
   fontSize: 36,
   fontWeight: '700',
   lineHeight: 1.11,
   letterSpacing: '-0.03em',
-  textAlign: 'left',
   marginBottom: 10,
 }
 const secondary = {
   fontSize: 16,
   fontWeight: '400',
   lineHeight: 1.625,
-  letterSpacing: '0em',
-  textAlign: 'left',
+  letterSpacing: 0,
   color: '#6B7280',
   marginBottom: 4,
 }
