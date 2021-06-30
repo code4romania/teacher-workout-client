a8ff9cbea6037b435f246f55a66730dca75e1a4b
Radu B <radu.buciuceanu@gmail.com>
2021-06-30 17:56:53 +0300

    [feature-6] Configure spacing

--- src/home/cards/index.jsx
+++ src/home/cards/index.jsx
@@ -4,15 +4,13 @@ import { Box, Grid } from '@material-ui/core'
 import { BCard } from 'home/cards/b-card'
 import { ACard } from 'home/cards/a-card'
 
-export const Cards = () => (
-  <Box pt={7}>
-    <Grid container justify={'center'}>
-      <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
-        <BCard />
-      </Grid>
-      <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
-        <ACard />
-      </Grid>
+export const Cards = () => <Box pt={7} px={5}>
+  <Grid container justify={'center'}>
+    <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
+      <BCard />
     </Grid>
-  </Box>
-)
+    <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
+      <ACard />
+    </Grid>
+  </Grid>
+</Box>
