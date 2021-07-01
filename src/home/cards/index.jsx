9bae6c880bce32bfafe448259445dd53d7790583
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2021-07-01 15:22:32 +0300

    [feature-58] Change cards layout (#59)

--- src/home/cards/index.jsx
+++ src/home/cards/index.jsx
@@ -5,10 +5,10 @@ import { BCard } from 'home/cards/b-card'
 import { ACard } from 'home/cards/a-card'
 
 export const Cards = () => <Grid container justify={'center'}>
-  <Grid item xs={12} sm={12} md={11} lg={6} xl={6}>
+  <Grid item xs={12} sm={12} md={11} lg={11} xl={11}>
     <BCard />
   </Grid>
-  <Grid item xs={12} sm={12} md={11} lg={6} xl={6}>
+  <Grid item xs={12} sm={12} md={11} lg={12} xl={12}>
     <ACard />
   </Grid>
 </Grid>
