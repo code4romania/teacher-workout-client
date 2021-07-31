0d73f48d47d165243fe7c13e95e133ad7e28581c
Radu B <radu.buciuceanu@gmail.com>
2021-07-31 14:39:43 +0300

    [feature/9] Remove placeholder component

--- src/lessons/index.jsx
+++ src/lessons/index.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import { Layout } from 'layout'
 import { Top } from 'lessons/top'
+import { Grid } from 'lessons/grid'
 import { Filters } from 'lessons/filters'
 import { Behaviour } from 'lessons/behaviour'
-import { Placeholder } from 'lessons/placeholder'
-import { Grid } from 'lessons/grid'
 
 export const Lessons = () => <Layout>
   <Behaviour />
   <Top />
   <Filters />
   <Grid />
-  <Placeholder />
 </Layout>
