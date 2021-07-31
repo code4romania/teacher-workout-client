11f276b9af1fb5efa71519a6534a45ec93a15beb
Radu B <radu.buciuceanu@gmail.com>
2021-07-31 14:35:25 +0300

    [feature/9] Create placeholder component

---
+++ src/lessons/placeholder.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Box } from '@material-ui/core'
+
+export const Placeholder = () => <Box height={'70vh'} display={'flex'} alignItems={'center'} justifyContent={'center'}>
+  Lessons...
+</Box>
