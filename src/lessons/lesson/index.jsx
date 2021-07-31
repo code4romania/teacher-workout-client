1eabb3e411b50aedfb395d97250f475f71e4af34
Radu B <radu.buciuceanu@gmail.com>
2021-07-31 14:38:31 +0300

    [feature/9] Configure lesson responsiveness

--- src/lessons/lesson/index.jsx
+++ src/lessons/lesson/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import * as Material from '@material-ui/core'
 import { Card } from 'lessons/lesson/card'
+import * as Material from '@material-ui/core'
 
-export const Lesson = () => <Material.Grid item xs={4}>
+export const Lesson = () => <Material.Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
   <Material.Box p={3}>
     <Card />
   </Material.Box>
