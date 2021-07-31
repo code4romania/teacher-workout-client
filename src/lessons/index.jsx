1ad3fc31e1ff6bd8f29fa309cda6d07100ef39c1
Radu B <radu.buciuceanu@gmail.com>
2021-07-31 12:08:18 +0300

    [feature/9] Change lessons page

--- src/lessons/index.jsx
+++ src/lessons/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Layout } from 'layout'
+import { Top } from 'lessons/top'
 import { Box } from '@material-ui/core'
 
 export const Lessons = () => <Layout>
+  <Top />
   <Box height={'70vh'} display={'flex'} alignItems={'center'} justifyContent={'center'}>
     Lessons...
   </Box>
