1d181702e13d94017d3ce55f67b58132900d00bf
Radu B <radu.buciuceanu@gmail.com>
2021-07-31 12:15:56 +0300

    [feature/9] Change top layout

--- src/lessons/top/layout.jsx
+++ src/lessons/top/layout.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { Grid } from '@material-ui/core'
 
 export const Layout = ({ children }) => <Grid container justify={'center'}>
-  <Grid item xs={12} sm={10} md={6} lg={5} xl={5}>
+  <Grid item xs={12} sm={10} md={6} lg={6} xl={6}>
     {children}
   </Grid>
 </Grid>
