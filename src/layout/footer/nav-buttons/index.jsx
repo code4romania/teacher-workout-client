eca778c21f00c46c954dc174b7bd1b5c0c14c845
Crisan Catalin <catacrisan_catacrsn@yahoo.com>
2021-08-01 12:45:32 +0300

    [feature/63] Configure footer links (#72)
    Co-authored-by: Catalin <c>

--- src/layout/footer/nav-buttons/index.jsx
+++ src/layout/footer/nav-buttons/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { makeStyles, Box, Grid } from '@material-ui/core'
 
-import { About } from 'layout/footer/nav-buttons/about'
-import { Donate } from 'layout/footer/nav-buttons/donate'
 import { PrivacyPolicy } from 'layout/footer/nav-buttons/privacy-policy'
 import { Terms } from 'layout/footer/nav-buttons/terms'
+import { About } from 'layout/common/about'
+import { Donate } from 'layout/common/donate'
 
 const useStyles = makeStyles(theme => ({
   navButtons: {
--- src/layout/header/desktop-buttons/index.jsx
+++ src/layout/header/desktop-buttons/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Grid, Hidden } from '@material-ui/core'
 
-import { About } from 'layout/header/desktop-buttons/about'
-import { Donate } from 'layout/header/desktop-buttons/donate'
 import { Contact } from 'layout/header/desktop-buttons/contact'
 import { Lessons } from 'layout/header/desktop-buttons/lessons'
+import { About } from 'layout/common/about'
+import { Donate } from 'layout/common/donate'
 
 export const DesktopButtons = () => <Hidden smDown>
   <Grid item container justify={'center'} alignItems={'center'}>
