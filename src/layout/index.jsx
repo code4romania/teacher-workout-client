b4312a32e5a73e4907c1c02521890a956ff78192
Radu B <radu.buciuceanu@gmail.com>
2021-08-01 20:32:14 +0300

    [bug/64] Change layout component

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Box, makeStyles } from '@material-ui/core'
-
 import { Header } from 'layout/header'
 import { Footer } from 'layout/footer'
 import { Spacing } from 'layout/common/spacing'
+import { Box, makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({ root: { background: theme.palette.background.main } }))
 
