6e4a091790a4ff00928a55a11259804bf358db2d
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2021-08-28 18:39:33 +0300

    [feature/81] Use admin navbar (#101)

--- src/admin/layout/hooks/use-styles.js
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from '@material-ui/core'
-
-export const useStyles = makeStyles(theme => ({
-  root: {
-    padding: theme.spacing(10),
-    [theme.breakpoints.only('xs')]: { padding: theme.spacing(0) },
-  },
-}))
