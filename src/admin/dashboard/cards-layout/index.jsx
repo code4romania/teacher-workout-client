1b2aa9a58c2a37565cd0058e50c7349b8d2ab774
Casuneanu Catalin <catalin.casuneanu@gmail.com>
2021-08-28 20:42:58 +0300

    fix:lint

--- src/admin/dashboard/cards-layout/index.jsx
+++ src/admin/dashboard/cards-layout/index.jsx
@@ -1,37 +1,32 @@
-import React from "react";
+/* eslint-disable max-lines-per-function */
+import React from 'react'
 import * as Material from '@material-ui/core'
-import { staticData } from "admin/dashboard/staticData"
-import { Card } from "admin/dashboard/card";
+import { staticData } from 'admin/dashboard/static-data'
+import { Card } from 'admin/dashboard/card'
 
-export const CardsLayout = () => <>
-<Material.Grid item container xs={12} spacing={2} direction="row">
-    {staticData
-    .filter((data, idx) => idx < 3)
-    .map((data, idx) => 
-        <Material.Grid item xs={12} md={4} key={idx}>
-            <Card 
-                icon={data.icon} 
+export const CardsLayout = () => (
+  <>
+    <Material.Grid item container xs={12} spacing={2} direction={'row'}>
+      {staticData
+        .filter((data, idx) => idx < 3)
+        .map((data, idx) => <Material.Grid item xs={12} md={4} key={idx}>
+          <Card icon={data.icon} 
                 statistics={data.statistics} 
                 title={data.title} 
-                link={data.link} 
-                linkText={data.linkText}
-            />
-        </Material.Grid>
-    )}
-</Material.Grid>
-<Material.Grid item container xs={12} spacing={2} direction="row">
-    {staticData
-    .filter((data, idx) => idx >= 3)
-    .map((data, idx) => 
-        <Material.Grid item xs={12} md={6} key={idx}>
-            <Card 
-                icon={data.icon} 
+                link={data.link}
+                linkText={data.linkText} />
+        </Material.Grid>)}
+    </Material.Grid>
+    <Material.Grid item container xs={12} spacing={2} direction={'row'}>
+      {staticData
+        .filter((data, idx) => idx >= 3)
+        .map((data, idx) => <Material.Grid item xs={12} md={6} key={idx}>
+          <Card icon={data.icon} 
                 statistics={data.statistics} 
                 title={data.title} 
-                link={data.link} 
-                linkText={data.linkText}
-            />
-        </Material.Grid>
-    )}
-</Material.Grid>
-</>
+                link={data.link}
+                linkText={data.linkText} />
+        </Material.Grid>)}
+    </Material.Grid>
+  </>
+)
--- src/admin/dashboard/title/index.jsx
+++ src/admin/dashboard/title/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import * as Material from '@material-ui/core'
 
-export const Title = () => {
-    return(
-    <Material.Box display={'inline-block'}>
-        <Material.Typography variant={'h4'}>
-            <strong>Dashboard</strong>
-        </Material.Typography>
-    </Material.Box>
-)}
+export const Title = () => (
+  <Material.Box display={'inline-block'}>
+    <Material.Typography variant={'h4'}>
+      <strong>Dashboard</strong>
+    </Material.Typography>
+  </Material.Box>
+)
