be5872566d0315c32b136aa187782bf2da1455c3
Radu B <radu.buciuceanu@gmail.com>
2021-08-28 17:47:59 +0300

    [feature/86] Create categories table

---
+++ src/admin/categories/table/body.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import * as Material from '@material-ui/core'
+import { Row } from 'admin/categories/table/row'
+import { useData } from 'admin/categories/table/hooks/use-data'
+
+export const Body = () => <Material.TableBody>
+  {useData().map(item => <Row key={JSON.stringify(item)} item={item} />)}
+</Material.TableBody>
---
+++ src/admin/categories/table/headers/count.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import * as Material from '@material-ui/core'
+
+export const Count = () => <Material.TableCell>
+  NUMAR DE LECTII
+</Material.TableCell>
