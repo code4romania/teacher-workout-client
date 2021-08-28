d6babf63ba60523c1a615138afce7a89f88e7541
Radu B <radu.buciuceanu@gmail.com>
2021-08-28 21:11:16 +0300

    [feature/105] Configure categories table

--- src/admin/categories/table/columns/count.jsx
+++ src/admin/categories/table/columns/count.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import * as Material from '@material-ui/core'
 
-export const Count = ({ item }) => <Material.TableCell>
-  {item.name}
-</Material.TableCell>
+export const Count = ({ item }) => <Material.Hidden smDown>
+  <Material.TableCell>
+    {item.name}
+  </Material.TableCell>
+</Material.Hidden>
 
 Count.propTypes = { item: PropTypes.object.isRequired }
