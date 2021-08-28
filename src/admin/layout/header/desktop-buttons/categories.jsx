ba2b831d310acc0f65c009b4c6ffc3a532024b0a
Radu B <radu.buciuceanu@gmail.com>
2021-08-28 18:15:56 +0300

    [feature-81] Don't use empty space

--- src/admin/layout/header/desktop-buttons/categories.jsx
+++ src/admin/layout/header/desktop-buttons/categories.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Box, Grid } from '@material-ui/core'
 import { Button } from 'admin/layout/button'
+import { Box, Grid } from '@material-ui/core'
 import { actions } from 'admin/layout/header/constants'
 
 export const Categories = () => <Grid item>
   <Box display={'flex'} justifyContent={'center'} py={1} px={1}>
     <Button>
-      {' '}
       {actions.categories}
-      {' '}
     </Button>
   </Box>
 </Grid>
--- src/admin/layout/header/desktop-buttons/users.jsx
+++ src/admin/layout/header/desktop-buttons/users.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Box, Grid } from '@material-ui/core'
 import { Button } from 'admin/layout/button'
+import { Box, Grid } from '@material-ui/core'
 import { actions } from 'admin/layout/header/constants'
 
 export const Users = () => <Grid item>
   <Box display={'flex'} justifyContent={'center'} py={1} px={1}>
     <Button>
-      {' '}
       {actions.users}
-      {' '}
     </Button>
   </Box>
 </Grid>
