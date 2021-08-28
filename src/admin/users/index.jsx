e5d038d5b1192ef600a819771de14e42e4556354
Radu B <radu.buciuceanu@gmail.com>
2021-08-28 15:34:43 +0300

    [feature/87] Use the new component

--- src/admin/users/index.jsx
+++ src/admin/users/index.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
 import { Title } from 'admin/users/title'
+import { Table } from 'admin/users/table'
 
-export const Users = () => <Title />
+export const Users = () => <>
+  <Title />
+  <Table />
+</>
