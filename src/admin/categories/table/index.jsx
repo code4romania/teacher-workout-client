69949f8b48cc45137ed2f7c34919d5bb414251c7
Radu B <radu.buciuceanu@gmail.com>
2021-08-28 18:51:59 +0300

    [feature/86] Use categories title

--- src/admin/categories/table/index.jsx
+++ src/admin/categories/table/index.jsx
@@ -3,7 +3,7 @@ import * as Material from '@material-ui/core'
 import { Head } from 'admin/categories/table/head'
 import { Body } from 'admin/categories/table/body'
 
-export const Table = () => <Material.Box pt={5}>
+export const Table = () => <Material.Box pt={3}>
   <Material.Table>
     <Head />
     <Body />
