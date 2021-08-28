5b8e4ce4561796f10d56dfe8ffac9db75d72a7ec
Radu B <radu.buciuceanu@gmail.com>
2021-08-28 18:52:38 +0300

    [feature/86] Configure users spacings

--- src/admin/users/title/header.jsx
+++ src/admin/users/title/header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as Material from '@material-ui/core'
 
-export const Header = () => <Material.Box display={'inline-block'}>
+export const Header = () => <Material.Box display={'inline-block'} pt={6}>
   <Material.Typography variant={'h4'}>
     <strong>Utilizatori</strong>
   </Material.Typography>
