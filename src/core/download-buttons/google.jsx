de543fc21aaf47d1635e25367952106580f4c586
radu <radu.buciuceanu@gmail.com>
2021-09-25 15:23:45 +0300

    [enh/120.1] Move download buttons

--- src/core/download-buttons/google.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Image } from 'core/download-buttons/image'
-import { Button } from 'core/download-buttons/button'
-import Source from 'core/download-buttons/google.svg'
-
-export const Google = () => <Button>
-  <Image src={Source} />
-</Button>
