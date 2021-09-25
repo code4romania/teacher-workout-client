867c57e0985e715ced099dc72bc4eed2eaecf116
radu <radu.buciuceanu@gmail.com>
2021-09-25 14:16:34 +0300

    [feature/112] Change dashboard page

--- src/admin/dashboard/index.jsx
+++ src/admin/dashboard/index.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import * as Material from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-import { Title } from 'admin/dashboard/title'
 import { Layout } from 'admin/layout'
-import { CardsLayout } from 'admin/dashboard/cards-layout'
-
-const useStyles = makeStyles({ wrapper: { paddingTop: '33px' } })
+import { Title } from 'admin/dashboard/title'
+import { Cards } from 'admin/dashboard/cards'
 
 export const Dashboard = () => <Layout>
-  <Material.Grid container className={useStyles().wrapper} direction={'column'} spacing={3}>
-    <Material.Grid item>
-      <Title />
-    </Material.Grid>
-    <CardsLayout />
-  </Material.Grid>
+  <Title />
+  <Cards />
 </Layout>
+
