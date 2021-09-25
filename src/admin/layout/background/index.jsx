3e7d5fa4acd6e79e46ced7ebb4408e8dab8a6f10
radu <radu.buciuceanu@gmail.com>
2021-09-25 14:32:51 +0300

    [bug/114] Solve dashboard on mobile

--- src/admin/layout/background/index.jsx
+++ src/admin/layout/background/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import BackgroundPattern from 'admin/layout/background/background-pattern.svg'
+import { ReactComponent as Svg } from 'admin/layout/background/background.svg'
 
 const useStyles = makeStyles({
   image: {
-    position: 'absolute',
+    position: 'fixed',
     zIndex: '-1',
+    bottom: 0,
+    left: 0,
   },
 })
 
-export const Background = () => <img src={BackgroundPattern} alt={'background pattern'} className={useStyles().image} />
+export const Background = () => <Svg className={useStyles().image} />
