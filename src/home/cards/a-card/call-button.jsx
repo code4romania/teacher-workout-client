8da8f65d365b8b71c1a234995deac62047a2e1b2
radu <radu.buciuceanu@gmail.com>
2021-09-25 15:18:38 +0300

    [enhancement/120] Change a card

--- src/home/cards/a-card/call-button.jsx
+++ src/home/cards/a-card/call-button.jsx
@@ -1,25 +1,9 @@
 import React from 'react'
-import { Button, makeStyles } from '@material-ui/core'
-import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
+import * as Core from 'core/buttons'
+import * as Icons from '@material-ui/icons'
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    fontFamily: theme.typography.fontFamily,
-    fontStyle: 'normal',
-    fontWeight: 'bold',
-    fontSize: '16px',
-    lineHeight: 1.5,
-    color: '#FFFFFF',
-    marginTop: theme.spacing(3),
-    textTransform: 'none',
-  },
-}))
-export const CallButton = () => (
-  <Button variant={'contained'}
-          color={'primary'}
-          size={'small'}
-          className={useStyles().button}
-          endIcon={<ArrowForwardIcon />}>
-    Call to action ptr creatori
-  </Button>
-)
+export const CallButton = () => <Core.Buttons.Blue>
+  Call to action
+  <Icons.ArrowForward fontSize={'small'} />
+</Core.Buttons.Blue>
+
