2635fdb657ca61c75ef13564921e26464e237404
radu <radu.buciuceanu@gmail.com>
2021-09-25 14:43:58 +0300

    [bug/116] Configure table on categories

--- src/admin/categories/table/index.jsx
+++ src/admin/categories/table/index.jsx
@@ -3,7 +3,9 @@ import * as Material from '@material-ui/core'
 import { Head } from 'admin/categories/table/head'
 import { Body } from 'admin/categories/table/body'
 
-export const Table = () => <Material.Box pt={3}>
+const useStyles = Material.makeStyles({ root: { background: 'white' } })
+
+export const Table = () => <Material.Box pt={3} className={useStyles().root}>
   <Material.Table>
     <Head />
     <Body />
