e36eda87fac5b183477d3266abaeeed69db4b737
radu <radu.buciuceanu@gmail.com>
2021-09-25 14:44:25 +0300

    [bug/116] Configure table on users

--- src/admin/users/table/index.jsx
+++ src/admin/users/table/index.jsx
@@ -3,7 +3,9 @@ import * as Material from '@material-ui/core'
 import { Head } from 'admin/users/table/head'
 import { Body } from 'admin/users/table/body'
 
-export const Table = () => <Material.Box pt={3}>
+const useStyles = Material.makeStyles({ root: { background: 'white' } })
+
+export const Table = () => <Material.Box pt={3} className={useStyles().root}>
   <Material.Table>
     <Head />
     <Body />
