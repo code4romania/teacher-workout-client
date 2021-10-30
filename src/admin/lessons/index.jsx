cc4106c29143ac5a7500bd34207f7506b749d53f
radu <radu.buciuceanu@gmail.com>
2021-10-30 15:00:04 +0300

    [feature/85] Use table component

--- src/admin/lessons/index.jsx
+++ src/admin/lessons/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Layout } from 'admin/layout'
 import { Title } from 'admin/lessons/title'
+import { Table } from 'admin/lessons/table'
 import { Filters } from 'admin/lessons/filters'
 
 export const Lessons = () => <Layout>
   <Title />
   <Filters />
+  <Table />
 </Layout>
