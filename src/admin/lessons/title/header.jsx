04c10cf4ab3ae54968fa6375fc629ce0b3a0a0ea
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2021-10-30 14:53:50 +0300

    [feature/85] Solve lessons title typo (#136)

--- src/admin/lessons/title/header.jsx
+++ src/admin/lessons/title/header.jsx
@@ -3,6 +3,6 @@ import * as Material from '@material-ui/core'
 
 export const Header = () => <Material.Box display={'inline-block'} pt={6}>
   <Material.Typography variant={'h4'}>
-    <strong>Utilizatori</strong>
+    <strong>Listă lecții</strong>
   </Material.Typography>
 </Material.Box>
