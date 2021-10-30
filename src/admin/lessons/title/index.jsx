4c26c97e6c70f825776aa5413d11315af6ac59db
radu <radu.buciuceanu@gmail.com>
2021-10-30 11:22:34 +0300

    [feature/85] Use count in lessons title

--- src/admin/lessons/title/index.jsx
+++ src/admin/lessons/title/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import * as Material from '@material-ui/core'
+import { Count } from 'admin/lessons/title/count'
 import { Header } from 'admin/lessons/title/header'
 
 export const Title = () => <Material.Grid container alignItems={'flex-end'}>
   <Material.Grid item xs={9}>
     <Header />
+    <Count />
   </Material.Grid>
 </Material.Grid>
-
