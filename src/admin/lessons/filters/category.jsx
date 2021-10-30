c02e066724f8ba1c3327c00bccd19bc57af34c0d
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2021-10-30 15:31:53 +0300

    [feature/85] Add select component to core (#139)

--- src/admin/lessons/filters/category.jsx
+++ src/admin/lessons/filters/category.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Select } from 'core/select'
+import { Core } from 'core'
 import * as Material from '@material-ui/core'
 import { useStyles } from 'admin/lessons/filters/hooks/use-styles'
 
@@ -10,6 +10,6 @@ const OPTIONS = Object.freeze([
 
 export const Category = () => <Material.Grid item xs={12} md={4}>
   <Material.Box pr={3} className={useStyles().box}>
-    <Select title={'Filtrează după categorie'} options={OPTIONS} />
+    <Core.Select title={'Filtrează după categorie'} options={OPTIONS} />
   </Material.Box>
 </Material.Grid>
