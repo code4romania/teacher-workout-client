4100595642ce7b1a37700772f9abd64964c119ba
radu <radu.buciuceanu@gmail.com>
2021-10-30 15:33:51 +0300

    [feature/140] Solve admin layout error

--- src/admin/layout/header/bar/notifications.jsx
+++ src/admin/layout/header/bar/notifications.jsx
@@ -5,7 +5,7 @@ import * as Material from '@material-ui/core'
 export const Notifications = () => <Material.Hidden smDown>
   <Material.Grid item>
     <Material.Box display={'flex'} justifyContent={'center'} pl={3} color={'#9CA3AF'}>
-      <Icons.NotificationsOutlined fontSize={'normal'} />
+      <Icons.NotificationsOutlined fontSize={'default'} />
     </Material.Box>
   </Material.Grid>
 </Material.Hidden>
