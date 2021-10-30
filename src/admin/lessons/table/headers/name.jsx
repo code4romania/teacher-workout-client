cf9cf0a9069e54e36122fbd360aecf50962cde94
radu <radu.buciuceanu@gmail.com>
2021-10-30 14:58:06 +0300

    [feature/85] Create table headers

---
+++ src/admin/lessons/table/headers/name.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import * as Material from '@material-ui/core'
+
+export const Name = () => <Material.TableCell>
+  DENUMIRE LECȚIE
+</Material.TableCell>
---
+++ src/admin/lessons/table/headers/status.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import * as Material from '@material-ui/core'
+
+export const Status = () => <Material.Hidden smDown>
+  <Material.TableCell>
+    STATUS
+  </Material.TableCell>
+</Material.Hidden>
