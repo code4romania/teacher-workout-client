0eb75a40b64c017196d3359b5dd6b877ce5e3710
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2021-10-30 11:26:21 +0300

    [feature/85] Use count in lessons title (#130)

---
+++ src/admin/lessons/title/count.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import * as Material from '@material-ui/core'
+
+export const Count = () => <Material.Box pb={0.3} pl={1} display={'inline-block'}>
+  <Material.Typography color={'primary'}>
+    <strong>(415 lecții)</strong>
+  </Material.Typography>
+</Material.Box>
