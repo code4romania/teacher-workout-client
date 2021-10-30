7808215d9630bb472bac3a02c560057d153bb8c9
radu <radu.buciuceanu@gmail.com>
2021-10-30 11:02:49 +0300

    [feature/85] Use layout in admin lessons

--- src/admin/lessons/index.jsx
+++ src/admin/lessons/index.jsx
@@ -1,1 +1,11 @@
-export const Lessons = () => 'Lessons'
+import React from 'react'
+import { Layout } from 'admin/layout'
+import * as Material from '@material-ui/core'
+
+export const Lessons = () => <Layout>
+  <Material.Box pt={3}>
+    <Material.Typography align={'center'}>
+      Lessons
+    </Material.Typography>
+  </Material.Box>
+</Layout>
