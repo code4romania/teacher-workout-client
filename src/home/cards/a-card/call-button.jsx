249201eb30c809acbc2eda7632385dbef3b73734
Ciprian Platica <platica.ciprian@gmail.com>
2021-10-30 17:55:22 +0300

    Fix button spacing on home

--- src/home/cards/a-card/call-button.jsx
+++ src/home/cards/a-card/call-button.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import * as Core from 'core/buttons'
 import * as Icons from '@material-ui/icons'
+import * as Material from '@material-ui/core'
 
-export const CallButton = () => <Core.Buttons.Blue>
-  Call to action
-  <Icons.ArrowForward fontSize={'small'} />
-</Core.Buttons.Blue>
+export const CallButton = () => <Material.Box mt={3}>
+  <Core.Buttons.Blue>
+    Call to action
+    <Icons.ArrowForward fontSize={'small'} />
+  </Core.Buttons.Blue>
+</Material.Box>
 
