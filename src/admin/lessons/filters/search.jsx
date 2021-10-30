d66c0673b92db8dee647ac5b96d6e06de0bebea7
radu <radu.buciuceanu@gmail.com>
2021-10-30 13:27:30 +0300

    [feature/85] Create filters on lessons

---
+++ src/admin/lessons/filters/search.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import * as Material from '@material-ui/core'
+
+export const Search = () => <Material.Grid item xs={12} md={4}>
+  <Material.TextField fullWidth label={'Caută după cuvinte cheie'} />
+</Material.Grid>
---
+++ src/admin/lessons/filters/status.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Select } from 'core/select'
+import * as Material from '@material-ui/core'
+import { useStyles } from 'admin/lessons/filters/hooks/use-styles'
+
+const OPTIONS = Object.freeze([
+  { value: 'one', label: 'One' },
+  { value: 'two', label: 'Two' },
+])
+
+export const Status = () => <Material.Grid item xs={12} md={4}>
+  <Material.Box pr={3} className={useStyles().box}>
+    <Select title={'Filtrează după status'} options={OPTIONS} />
+  </Material.Box>
+</Material.Grid>
