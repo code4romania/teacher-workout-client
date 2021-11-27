ff09ce9bac43042d1dadb5ae336ea55edfa308b8
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2021-11-27 18:37:55 +0200

    [feature/125] Solve same key warning (#146)

--- src/admin/categories/table/body.jsx
+++ src/admin/categories/table/body.jsx
@@ -4,5 +4,5 @@ import { Row } from 'admin/categories/table/row'
 import { useData } from 'admin/categories/table/hooks/use-data'
 
 export const Body = () => <Material.TableBody>
-  {useData().map(item => <Row key={JSON.stringify(item)} item={item} />)}
+  {useData().map((item, index) => <Row key={index} item={item} />)}
 </Material.TableBody>
