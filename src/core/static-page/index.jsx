da400833e6aa8ee6e37fd5a172777fcb82684f2a
dasilvaluis <15066801+dasilvaluis@users.noreply.github.com>
2022-05-28 14:19:26 +0300

    153: Fix style for page title

--- src/core/static-page/index.jsx
+++ src/core/static-page/index.jsx
@@ -4,28 +4,59 @@ import parse from 'html-react-parser'
 import { makeStyles } from '@mui/styles'
 import { Container } from '@mui/material'
 
-const useStyles = makeStyles({
-  container: {
-    '& a': { '&:link, &:hover, &:visited, &:active': { color: '#ec203a', textDecoration: 'none' } },
-    '& h1': { marginBottom: '71px' },
-    '& h3': { marginBottom: '20px' },
-    '& p': { marginBottom: '30px' },
-    '& p + ul': { marginBottom: '20px' },
-    '& ul + p': { marginTop: '26px' },
-    '& li': { marginBottom: '18px' },
-    '& ul': { listStyle: 'none' },
-    '& ul li': {
-      '&::before': {
-        content: '\'\\2022\'',
-        color: '#E5E7EB',
-        fontWeight: 'bold',
-        display: 'inline-block',
-        width: '1em',
-        marginLeft: '-1em',
-      },
+const makeTitleStyles = (theme) => ({
+  '& h1': {
+    fontSize: '52px',
+    marginTop: '40px',
+    marginBottom: '47px',
+
+    [theme.breakpoints.up('md')]: {
+      marginTop: '60px',
+      marginBottom: '71px',
+    }
+  },
+  '& h3': { marginBottom: '20px' },
+});
+
+const makeListsStyles = () => ({
+  '& p + ul': { marginBottom: '20px' },
+  '& li': { marginBottom: '18px' },
+  '& ul': { listStyle: 'none' },
+  '& ul li': {
+    '&::before': {
+      content: '\'\\2022\'',
+      color: '#E5E7EB',
+      fontWeight: 'bold',
+      display: 'inline-block',
+      width: '1em',
+      marginLeft: '-1em',
     },
   },
-})
+});
+
+const makeParagraphStyles = () => ({
+  '& p': { marginBottom: '30px' },
+  '& ul + p': { marginTop: '26px' },
+});
+
+const makeAnchorStyles = () => ({
+  '& a': {
+    '&:link, &:hover, &:visited, &:active': {
+      color: '#ec203a',
+      textDecoration: 'none'
+    }
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    ...makeTitleStyles(theme),
+    ...makeListsStyles(),
+    ...makeParagraphStyles(),
+    ...makeAnchorStyles(),
+    fontFamily: theme.typography.fontFamily,
+  },
+}));
 
 export const StaticPage = props => (
   <div>
