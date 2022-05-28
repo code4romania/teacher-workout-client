8dbbd79ed2958b3d93239ad075c9e6a691552bbe
dasilvaluis <15066801+dasilvaluis@users.noreply.github.com>
2022-05-28 16:04:30 +0300

    155: Fix filters on mobile

--- src/admin/lessons/filters/index.jsx
+++ src/admin/lessons/filters/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import * as Material from '@mui/material'
+import { Grid, Box } from '@mui/material'
 import { Search } from 'admin/lessons/filters/search'
 import { Status } from 'admin/lessons/filters/status'
 import { Category } from 'admin/lessons/filters/category'
 
-export const Filters = () => <Material.Box py={3} width={1}>
-  <Material.Grid container>
-    <Category />
-    <Status />
-    <Search />
-  </Material.Grid>
-</Material.Box>
+export const Filters = () => (
+  <Box mt={5} mb={3} width={1}>
+    <Grid container columnSpacing={3} rowSpacing={2}>
+      <Category />
+      <Status />
+      <Search />
+    </Grid>
+  </Box>
+);
